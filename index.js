aee0d492366bffb4f2fcac29e55bf6cec67fbf90
andrade._.g <gabriel.vinicius.andrade@escola.pr.gov.br>
2026-06-22 08:56:50 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //npm init 
 //npm i express 
 //cliente rapidAPI
-//http://localhilost:3000/perf
+//http://localhost:3000/perf
 
 
 const express = require("express")
@@ -37,7 +37,6 @@ app.get("/ola", (req, res)=>{
     })
 })
 
-
 app.get("/clientes", (req, res)=>{
     try {
         //abrir arquivo
@@ -49,7 +48,57 @@ app.get("/clientes", (req, res)=>{
 })
 
 
-app.listen(port, ()=>{
-    console.log("API executando na porta "+ port)
-})
 
+app.get("/clientes/cpf/:cpf", (req, res)=>{
+        const cpf =req.params.cpf
+        try {
+            const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
+            const cliente = bd.find((cliente) => cliente.cpf==cpf)
+            if(!cliente) {
+                return res.status(404).json({erro:"cliente não existe no BD, digite novamente!!!"})
+            }
+        
+
+            res.status(200).json({resposta: cliente})
+        }catch (erro){
+            res.status(500).json({erro: erro.message})
+        }
+    })
+    
+
+app.get("/clientes/nome/:nome", (req, res)=>{
+        const nome =req.params.nome
+        try {
+            const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
+            const cliente = bd.find((cliente) => cliente.nome==nome)
+            if(!cliente) {
+                return res.status(404).json({erro:"cliente não existe no BD, digite novamente!!!"})
+            }
+
+
+            res.status(200).json({resposta: cliente})
+        }catch (erro){
+            res.status(500).json({erro: erro.message})
+        }
+    })
+    
+
+    app.get("/clientes/data_nasc/:data_nasc", (req, res)=>{
+        const data_nasc =req.params.data_nasc
+        try {
+            const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
+            const cliente = bd.find((cliente) => cliente.data_nasc==data_nasc)
+            if(!cliente) {
+                return res.status(404).json({erro:"cliente não existe no BD, digite novamente!!!"})
+            }
+        
+
+            res.status(200).json({resposta: cliente})
+        }catch (erro){
+            res.status(500).json({erro: erro.message})
+        }
+    })
+
+app.listen(port, ()=>{
+        console.log("API executando na porta "+ port)
+})
